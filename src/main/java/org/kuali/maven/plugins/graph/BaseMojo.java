12ebfc495e993f742608ed39b59292792ba45f7d
Jeff Caddel <jcaddel@yahoo.com>
2011-12-19 19:21:20 -0700

    ECL license

--- src/main/java/org/kuali/maven/plugins/graph/BaseMojo.java
+++ src/main/java/org/kuali/maven/plugins/graph/BaseMojo.java
@@ -1,12 +1,11 @@
 /**
- * Copyright (C) 2009 Progress Software, Inc.
- * http://fusesource.com
+ * Copyright 2004-2011 The Kuali Foundation
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Educational Community License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *    http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.opensource.org/licenses/ecl2.php
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
