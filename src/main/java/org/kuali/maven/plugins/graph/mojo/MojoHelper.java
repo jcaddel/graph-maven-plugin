0df8052a42538bb320234bbbbdc669744bfdec8a
Jeff Caddel <jcaddel@yahoo.com>
2012-01-07 11:09:39 -0700

    fix typo

--- src/main/java/org/kuali/maven/plugins/graph/mojo/MojoHelper.java
+++ src/main/java/org/kuali/maven/plugins/graph/mojo/MojoHelper.java
@@ -211,7 +211,7 @@ public class MojoHelper {
 
     protected String getDescription(boolean transitive) {
         if (transitive) {
-            return "These are the dependencies declared in this project's pom plus any transitive dependencies of those dependenies.";
+            return "These are the dependencies declared in this project's pom plus any transitive dependencies of those dependencies.";
         } else {
             return "These are the dependencies declared in this project's pom";
         }
